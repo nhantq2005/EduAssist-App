3cd71f877e34003bd80435a60bf6ff9bb5d8200c
nhantq <nhan.tq2005@gmail.com>
2026-08-17 19:16:46 +0700

    Add EditUser screen and ChatStyle for messaging interface

---
+++ src/screens/auth/EditUser.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native-paper"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const EditUser = () => {
+    return (
+        <SafeAreaView>
+            <Text>Edit User</Text>
+        </SafeAreaView>
+    )
+}
+
+export default EditUser
